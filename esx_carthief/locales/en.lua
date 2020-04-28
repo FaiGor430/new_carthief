90150a902564a82e902b4ec0bdeaa9c6542fbce7
TerryWong8 <63836462+TerryWong8@users.noreply.github.com>
2020-04-28 18:54:13 +0800

    Add files via upload

---
+++ esx_carthief/locales/en.lua
@@ -0,0 +1,13 @@
+Locales['en'] = {
+  ['vehicle_robbery'] = '走私車輛',
+  ['mission_failed'] = '任務失敗!',
+  ['get_back_car_10s'] = '您有10秒鐘的時間回到車裡',
+  ['get_back_car_1m'] = '你有1分鐘的時間回到車裡',
+  ['car_provided_rule'] = '您必須使用為您提供的汽車，並且您必須停下來.',
+  ['already_robbery'] = '已經有搶劫案在進行中!',
+  ['not_enough_cops'] = '鎮上沒有足夠的警察!',
+  ['car_stealing_in_progress'] = '偷車正在進行中。車輛追踪器將在您的雷達上處於活動狀態',
+  ['steal_a_car'] = '按 ~INPUT_CONTEXT~ 偷車',
+  ['drop_car_off'] = '按 ~INPUT_CONTEXT~ 下車',
+  ['cooldown'] = '最近發生了一起汽車搶劫案。等待 %s 秒才能偷走另一個.'
+}
